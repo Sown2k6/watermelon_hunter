095e70497080264245c6ebb0d5f35482f14e25fd
sown2k6 <sown2k6@gmail.com>
2025-02-28 15:08:05 +0700

    movements

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # word-catcher
+
+## How to play
+- Use the A key to move to the left
+- Use the D key to move to the right
+- Use the W key to jump
+
+## Gameplay
+- random words is generated
+- random letter fall down from the sky
+- the player will have to control the character to catch the letter and build the required words
--- main.cpp
+++ main.cpp
@@ -1,1 +1,92 @@
+#include <SDL2/SDL.h>
 #include <iostream>
+
+using namespace std;
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+// Kích thước nhân vật
+const int CHARACTER_WIDTH = 50;
+const int CHARACTER_HEIGHT = 50;
+
+// Hàm khởi tạo SDL
+bool init(SDL_Window*& window, SDL_Renderer*& renderer) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << endl;
+        return false;
+    }
+
+    window = SDL_CreateWindow("Word Catcher", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        cout << "Window could not be created! SDL_Error: " << SDL_GetError() << endl;
+        return false;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr) {
+        cout << "Renderer could not be created! SDL_Error: " << SDL_GetError() << endl;
+        return false;
+    }
+
+    return true;
+}
+
+// Hàm đóng SDL
+void close(SDL_Window* window, SDL_Renderer* renderer) {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+int main(int argc, char* args[]) {
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+
+    if (!init(window, renderer)) {
+        cout << "Failed to initialize!" << endl;
+        return -1;
+    }
+
+    // Vị trí ban đầu của nhân vật
+    int characterPosX = (SCREEN_WIDTH - CHARACTER_WIDTH) / 2;
+    int characterPosY = SCREEN_HEIGHT - CHARACTER_HEIGHT - 50;
+
+    bool quit = false;
+    SDL_Event e;
+
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
+        if (currentKeyStates[SDL_SCANCODE_A]) {
+            characterPosX -= 1;
+        }
+        if (currentKeyStates[SDL_SCANCODE_D]) {
+            characterPosX += 1;
+        }
+
+        // màu nền
+        SDL_SetRenderDrawColor(renderer, 135, 206, 235, 255);
+        SDL_RenderClear(renderer);
+
+        // màu mặt đất
+        SDL_Rect platform = {0, SCREEN_HEIGHT - 50, SCREEN_WIDTH, 50};
+        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+        SDL_RenderFillRect(renderer, &platform);
+
+        // màu nhân vật
+        SDL_Rect character = {characterPosX, characterPosY, CHARACTER_WIDTH, CHARACTER_HEIGHT};
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &character);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    close(window, renderer);
+    return 0;
+}
